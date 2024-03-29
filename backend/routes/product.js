2af75c4a81e7c02da19c6a5f5681b828f46d6f9a
anaskadar01 <151867358+anaskadar01@users.noreply.github.com>
2024-03-29 14:50:34 +0100

    post

--- backend/routes/product.js
+++ backend/routes/product.js
@@ -28,9 +28,35 @@ router.get('/api/products/:productId', async (req, res) => {
 
 // You can use the authMiddleware with req.user.id to authenticate your endpoint ;)
 
-router.post('/api/products', (req, res) => {
-  res.status(600).send()
-})
+router.post('/api/products', authMiddleware, async (req, res) => {
+  try {
+    const { name, description, category, originalPrice, pictureUrl, endDate } = req.body;
+
+    if (!name || !description || !category || !originalPrice || !pictureUrl || !endDate) {
+      return res.status(400).json({ error: 'Invalid or missing fields', details: ['name', 'description', 'category', 'originalPrice', 'pictureUrl', 'endDate'] });
+    }
+
+    if (isNaN(originalPrice) || originalPrice <= 0) {
+      return res.status(400).json({ error: 'Le prix original doit être un nombre positif', details: ['originalPrice'] });
+    }
+
+    const newProduct = await Product.create({
+      name,
+      description,
+      category,
+      originalPrice,
+      pictureUrl,
+      endDate,
+      sellerId: req.user.id
+    });
+
+    res.status(201).json(newProduct);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: 'Erreur lors de l\'ajout du produit' });
+  }
+});
+
 
 router.put('/api/products/:productId', async (req, res) => {
   res.status(600).send()
